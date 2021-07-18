53eaebd6ac37370b1060f468387d5ebd26dca4df
Joseph Freeman <freemanjj1992@gmail.com>
2021-07-18 01:18:37 -0400

    functional

---
+++ dewCommands/index.js
@@ -0,0 +1,13 @@
+const dewList = [
+    "Well said fellow Dew Mate, and make sure to grab and ice cold Mountain Dew before gaslighting your friends!", 
+    "Dew what you think is best, but make sure you have an ice cold Mountain Dew in your hands!", 
+    "Dew you guys ever think about what your purpose is in life? Like, are we all just corporate shill's designed to please PepsiCo or is that just me?", 
+    "Pooty Push!!!!", 
+    "JMikey sus af", 
+    "If they're AFK, they're probably just grabbing another ice cold Mountain Dew!", 
+    "Skip on 7 but don't skip on the Mountain Dew!", 
+    "SodaOut", 
+    "You guys, I missed the 4/20 game and I've had FOMO ever since, I'll forver be chasing the potential high of being in that game, I am truly saddened by this and the fact that I am an unpaid laborer for PepsiCo."
+];
+
+module.exports = dewList;
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+require('dotenv').config();
+const dewList = require('./dewCommands/index');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('message', message => {
+    const returnMessage = dewList[Math.floor(Math.random() * dewList.length)];
+    if (message.content.toLowerCase().includes('do the dew')) {
+        message.channel.send(returnMessage);
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
